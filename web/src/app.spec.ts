c02e6b3a160a71be01bc6ec14c64ec1fc7857b79
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2019-12-18 14:53:49 +0100

    Add POST /todos

--- web/src/app.spec.ts
+++ web/src/app.spec.ts
@@ -1,14 +1,22 @@
 import request from "supertest";
 import { app } from "./app";
 
-describe("app", () => {
-  test("works", () =>
+describe("todos", () => {
+  test("handles get on index", () =>
     request(app)
       .get("/")
       .expect(200));
 
-  test("todos", () =>
-    request(app)
-      .get("/todos")
-      .expect(200));
+  test("handles get on index", async () => {
+    const response = await request(app).get("/todos");
+    expect(response.status).toBe(200);
+  })
+
+  test("#POST returns wthat ste", async () => {
+    const body = { title: "abc"};
+    const response = await request(app)
+      .post("/todos")
+      .send(body);
+    expect(response.body).toEqual(body);
+  })
 });
--- web/src/app.ts
+++ web/src/app.ts
@@ -1,9 +1,11 @@
 import cors from "cors";
 import express from "express";
+import bodyParser from "body-parser";
 
 export const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send();
@@ -12,3 +14,8 @@ app.get("/", (req, res) => {
 app.get("/todos", (req, res) => {
   res.send([]);
 });
+
+app.post("/todos", (req, res) => {
+  res.send(req.body);
+});
+
