9053a92d2196a98551d24f90f5827ece0bd69352
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2019-12-18 16:09:47 +0100

    add describes to test - refatcoring

--- web/src/app.spec.ts
+++ web/src/app.spec.ts
@@ -1,44 +1,58 @@
 import request from "supertest";
 import { app } from "./app";
+import { read } from "fs";
 
 describe("todos", () => {
-  test("handles get on index", () =>
-    request(app)
-      .get("/")
-      .expect(200));
+  beforeEach(() => request(app).delete("/todos"))
 
-  test("handles get on index", async () => {
-    const response = await request(app).get("/todos");
-    expect(response.status).toBe(200);
+  describe("#GET", () => {
+    test("handles get on index", () =>
+      request(app)
+        .get("/")
+        .expect(200));
+  
+    test("handles get on index", async () => {
+      const response = await request(app).get("/todos");
+      expect(response.status).toBe(200);
+    })
   })
 
-  test("#POST returns proper output", async () => {
-    const body = { title: "abc", completed: false};
-    const response = await request(app)
+  describe("#POST", () => {
+    test("#POST returns proper output", async () => {
+      const body = { title: "abc", completed: false};
+      const response = await request(app)
       .post("/todos")
       .send(body);
-    expect(response.body).toEqual(body);
-  })
-  
-  test("#DELETE returns status 200", async () => {
-    const response = await request(app).delete("/todos");
-    expect(response.status).toBe(204);
-  })
+      expect(response.body).toEqual(body);
+    })
 
-  test("handles delete", async () => {
-    await request(app).delete("/todos");
-    const response = await request(app).get("/todos");
-    expect(response.body.length).toBe(0);
-  })
-  
-  test("#POST creates new todos", async () => {
-    await request(app).post("/todos").send({ title: "new"})
-    const response = await request(app).get("/todos");
-    expect(response.body.length).toBe(1);
-  })  
+    test("#POST creates new todos", async () => {
+      await request(app).post("/todos").send({ title: "new"})
+      const response = await request(app).get("/todos");
+      expect(response.body.length).toBe(1);
+    }) 
 
-  test("#POST without title returns bad request", async () => {
-    const response = await request(app).post("/todos").send({})
-    expect(response.status).toBe(403);
+    test("#POST without title returns bad request", async () => {
+      const response = await request(app).post("/todos").send({})
+      expect(response.status).toBe(403);
+    })
+
+    test("#POST without title returns bad request", async () => {
+      const response = await request(app).post("/todos")
+      expect(response.status).toBe(403);
+    })
+  })
+    
+  describe('#DELETE all', () => {
+    test("#DELETE returns status 200", async () => {
+      const response = await request(app).delete("/todos");
+      expect(response.status).toBe(204);
+    })
+  
+    test("handles delete", async () => {
+      await request(app).delete("/todos");
+      const response = await request(app).get("/todos");
+      expect(response.body.length).toBe(0);
+    })
   })
 });
