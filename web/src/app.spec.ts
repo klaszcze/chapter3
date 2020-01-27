9890cc4d0d474e393e6429c308a1228d83e8098f
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2020-01-27 17:52:27 +0100

    extract requests to variables and use subjects in test

--- web/src/app.spec.ts
+++ web/src/app.spec.ts
@@ -1,10 +1,13 @@
 import request from "supertest";
 import { app } from "./app";
-import { read } from "fs";
 
 const getAllToDos = () => request(app).get("/todos");
+const getToDo = (id: number) => request(app).get(`/todos/${id}`)
 const createToDo = (body: { title: string, order?: number }) => request(app).post("/todos").send(body);
-const deleteAllTodos = () => request(app).delete("/todos")
+const deleteAllTodos = () => request(app).delete("/todos");
+const deleteToDo = (id: number) => request(app).delete(`/todos/${id}`)
+const updateToDo = (id: number,
+  body: { title?: string, order?: number, completed?: boolean }) => request(app).patch(`/todos/${id}`).send(body);
 
 describe("todos", () => {
   beforeEach(() => deleteAllTodos())
@@ -19,20 +22,20 @@ describe("todos", () => {
   })
 
   describe("#GET one", () => {
-    const subject = createToDo;
+    const subject = getToDo;
 
     test("GET with reurned url returns proper todo", async () => {
-      const todo = await subject({title: 'abc'});
-      const response = await request(app).get(`/todos/${todo.body.id}`); 
+      const todo = await createToDo({title: 'abc'});
+      const response = await subject(todo.body.id);
       expect(response.body.title).toEqual('abc')
     })
 
     test("Creating two todos", async () => {
-      const todo1 = await subject({title: 'abc'});
-      const todo2 = await subject({title: 'def'});
+      const todo1 = await createToDo({title: 'abc'});
+      const todo2 = await createToDo({title: 'def'});
       const response1 = await getAllToDos();
       expect(response1.body.length).toEqual(2);
-      const response2 = await request(app).get(`/todos/${todo2.body.id}`); 
+      const response2 = await subject(todo2.body.id);
       expect(response2.body.title).toEqual('def')
     })
   })
@@ -62,61 +65,65 @@ describe("todos", () => {
   })
   
   describe('#DELETE all', () => {
+    const subject = deleteAllTodos;
     test("#DELETE returns status 204", async () => {
-      const response = await deleteAllTodos();
+      const response = await subject();
       expect(response.status).toBe(204);
     })
     
     test("handles delete", async () => {
-      await deleteAllTodos();
+      await subject();
       const response = await getAllToDos();
       expect(response.body.length).toBe(0);
     })
   })
   
   describe("#DELETE one", () => {
+    const subject = deleteToDo
+
     test("#delete returns status 204", async () => {
       const todo = await createToDo({ title: "first" })
-      const response = await request(app).delete(`/todos/${todo.body.id}`)
+      const response = await subject(todo.body.id)
       expect(response.status).toBe(204);
     })
 
     test("handles delete of one todo", async () => {
       await createToDo({ title: "first" })
       const todo = await createToDo({ title: "second" })
-      await request(app).delete(`/todos/${todo.body.id}`);
+      await subject(todo.body.id);
       const response = await getAllToDos();
       expect(response.body.length).toBe(1);
     })
 
     test("Retrns status 403 in not found", async () => {
       const todo = await createToDo({ title: "first" })
-      const response = await request(app).delete(`/todos/${todo.body.id + 1}`)
+      const response = await subject(todo.body.id + 1)
       expect(response.status).toBe(403);
     })
   });
 
   describe("#PATCH", () => {
+    const subject = updateToDo
+
     test("#PATCH todo's title", async () => {
       const todo = await createToDo({ title: "first" })
-      await request(app).patch(`/todos/${todo.body.id}`).send({ title: "new" });
-      const response = await request(app).get(`/todos/${todo.body.id}`); 
+      await subject(todo.body.id, { title: "new" })
+      const response = await getToDo(todo.body.id);
       expect(response.body.title).toEqual("new")
     })
 
     test("#PATCH todo's completance", async () => {
       const todo = await createToDo({ title: "first" })
-      await request(app).patch(`/todos/${todo.body.id}`).send({ completed: true });
-      const response = await request(app).get(`/todos/${todo.body.id}`); 
+      await subject(todo.body.id, { completed: true });
+      const response = await getToDo(todo.body.id);
       expect(response.body.completed).toEqual(true)
     })
 
     test("#PATCH todo's order", async () => {
       const todo = await createToDo({ title: "first", order: 99 })
-      await request(app).patch(`/todos/${todo.body.id}`).send({ order: 11 });
-      const response = await request(app).get(`/todos/${todo.body.id}`); 
+      await subject(todo.body.id, { order: 11 });
+      const response = await getToDo(todo.body.id);
       expect(response.body.order).toEqual(11)
     })
   })
 });
-
